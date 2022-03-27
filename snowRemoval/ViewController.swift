9c1feec13ca5bace4923582b48fc23eded7934ad
tejveer2608 <tejihayer.92@gmail.com>
2022-03-27 00:17:23 -0400

    Initial Commit

---
+++ snowRemoval/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snowRemoval
+//
+//  Created by TEJVEER SINGH on 2022-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
